49938f002c4f9a581395973d5e264a61bde480bb
Treydog14 <Treydog2014@gmail.com>
2019-04-29 22:44:02 -0700

    Finished problems 4 and 5

--- Prob-4/Prob-4.py
+++ Prob-4/Prob-4.py
@@ -2,20 +2,19 @@
 #   Programming Assignment 3
 #     Prob-4.py
 
-# YOUR NAME
+# Trevor Bromley
+from random import *
 
 def main():
     print("\nTrevor's Output")
-
-    # write code that will sum the numbers between 15 and 30 inclusive
-    # and print the sum
     
-    # sum the numbers in the sequence [2, 4, 7, 9, 12, 14, 17, 19]
-    # and print the sum
+    sum = 0
+    for i in range(5):
+        num = randrange(1,100)
+        print(num)
+        sum = sum + num
+    print("---------")
+    print("sum: ", sum)
 
-    # use the randrange() function to sum 5 randomly generated numbers 
-    # between 1 and 99 inclusive.
-    # Print each random number on a line. 
-    # Print the sum on the last line
 
 main()
--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -5,13 +5,29 @@
 # Trevor Bromley
 
 def main():
-    x = 4
-    y = 1.50
-    z = 1.12
+    slice = 2.0
+    largeDrink = 1.5
+    donut = .56
+    sum = 0
+    print("Pizza @ 2:\t", slice * 2)
+    sum = slice * 2
+    print("Drink:\t\t", largeDrink)
+    sum = sum + largeDrink
+    print("Donut @ 2:\t", donut * 2)
+    sum = sum + donut
 
-    print("Pizza @ 2:\t", float(x))
-    print("Drink:\t\t", float(y))
-    print("Donut @ 2:\t", float(z))
+    print("---------")
 
+    tax = .56
+    print("Tax:\t\t", tax)
+    sum = sum + tax
+    print("\nTotal:\t\t", sum)
+    
+    print()
+    tendered = eval(input("Please enter an amount: "))
+    print("Tendered:\t{0:.2f} xyz {1:.2f}".format(tendered, sum))
+    change = tendered - sum
+    print("Change:\t\t{0:.2f}".format(change))
+    
 main()
 
